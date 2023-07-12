2919ad102577870b7c6de3d9a1dd1bdc394ebdae
yun Seonghuuk <dbstjdgur78@naver.com>
2023-07-12 17:35:29 +0900

    @One To One  단반향 테스트

--- src/main/java/com/sparta/jpaadvance/entity/User.java
+++ src/main/java/com/sparta/jpaadvance/entity/User.java
@@ -1,2 +1,16 @@
-package com.sparta.jpaadvance.entity;public class User {
+package com.sparta.jpaadvance.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
 }
---
+++ src/main/java/com/sparta/jpaadvance/repository/FoodRepository.java
@@ -0,0 +1,12 @@
+package com.sparta.jpaadvance.repository;
+
+import com.sparta.jpaadvance.entity.Food;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FoodRepository extends JpaRepository<Food, Long> {
+
+
+
+}
